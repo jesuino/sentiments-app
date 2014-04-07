b5e957f11d62cb828b5dd6cdba1771ea148c7636
William Antonio <william.a.siqueira@gmail.com>
2014-04-07 03:12:46 -0300

    Adding the view

--- src/main/java/org/jugvale/sentiments/view/App.java
+++ src/main/java/org/jugvale/sentiments/view/App.java
@@ -1,24 +1,89 @@
-package org.jugvale.sentiments;
+package org.jugvale.sentiments.view;
+
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+import javafx.collections.FXCollections;
+import javafx.event.ActionEvent;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.*;
+import javafx.scene.layout.VBox;
+import javafx.scene.chart.*;
+import org.jugvale.sentiments.service.TextSentimentService;
+import org.jugvale.sentiments.model.*;
+import java.util.*;
 
 public class App extends Application{
 
+	final TextSentimentService textSentimentService = new TextSentimentService();
+	final PieChart chart =  new PieChart();
+	final TableView<TextSentiment> table = new TableView<>();
+	final TextField txtQuery = new TextField();
+
+	public static void main(String args[]){
+		launch(args);
+	}
 
 	@Override
 	public void start(Stage stage){
-		Button btn = new Button(">> Click <<");
-		btn.setOnAction(e -> System.out.println("Hello JavaFX 8"));
-		StackPane root = new StackPane();
-		root.getChildren().add(btn);
+		VBox root = new VBox();
+		root.getChildren().addAll(txtQuery, chart, table);
 		stage.setScene(new Scene(root));
-		stage.setWidth(300);
-		stage.setHeight(300);
-		stage.setTitle("JavaFX 8 app");
+		stage.setWidth(400);
+		stage.setHeight(550);
+		stage.setTitle("Sentiments");
 		stage.show();
+		txtQuery.setOnAction(e -> {
+			List<TextSentiment> textSentiments = getTextSentiments(txtQuery.getText());
+			fillTable(textSentiments);
+			fillChart(textSentiments);
+			chart.setTitle("Sentiments for \"" + txtQuery.getText() +"\"");
+		});
+		txtQuery.setPromptText("Enter your query for text and press enter");
+		initializeApp();		
 	}
 
+	private void initializeApp(){
+		txtQuery.setText(null);
+		txtQuery.getOnAction().handle(null);
+		addTableColumns();
+	}
+	
+	private void addTableColumns(){
+		TableColumn<TextSentiment,String> textCol = new TableColumn<>("Text");
+		textCol.setCellValueFactory(new PropertyValueFactory("text"));
+		textCol.setMinWidth(200);
+		TableColumn<TextSentiment,String> polarityCol = new TableColumn<>("Polarity");
+		polarityCol.setCellValueFactory(new PropertyValueFactory("polarity"));
+		polarityCol.setMinWidth(80);
+		polarityCol.setResizable(false);
+		table.getColumns().setAll(textCol, polarityCol);
+	}
+
+	public void fillTable(List<TextSentiment> textSentiments){
+		table.setItems(FXCollections.observableArrayList(textSentiments));
+	}
+
+	public void fillChart(List<TextSentiment> textSentiments){
+		long polarity0Count = textSentiments.stream().filter(s -> s.getPolarity() == 0).count();
+		long polarity2Count = textSentiments.stream().filter(s -> s.getPolarity() == 2).count();
+		long polarity4Count = textSentiments.stream().filter(s -> s.getPolarity() == 4).count();
+		chart.setData(
+			FXCollections.observableArrayList(
+			new PieChart.Data(":(", polarity0Count),
+			new PieChart.Data(":|", polarity2Count),
+			new PieChart.Data(":)", polarity4Count)
+		));
+	}
+	public List<TextSentiment> getTextSentiments(String q){
+		return textSentimentService.getSentiments(query(q)).getTextSentiments();
+	}
+
+	public List<String> query(String q){
+		if(q == null)
+			return Arrays.asList("obama is awesome", "obama sucks", "obama eats potato");
+			// return something meaningful, like search twitter...
+		else
+			return null;
+	}
  }
